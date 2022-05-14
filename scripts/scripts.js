b2d201810f3cfbb54786755086728ea88187e003
pmcsantana <31020012+PeterssonMCS@users.noreply.github.com>
2022-05-13 23:57:34 -0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,4 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-
-// Create gradient
-var grd = ctx.createRadialGradient(500,500,0,500,500,200);
-grd.addColorStop(0, "white");
-grd.addColorStop(1, "purple");
-
-// Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(0, 0, 1000,1000);
+function mudarImagem(input){
+    document.getElementById("picture").src = input;
+}
+    
