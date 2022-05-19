fbf39cc5689f01e96a9f1072f14cea8d100abb5f
Petersson <petersson.mcs@gmail.com>
2022-05-19 13:11:02 -0300

    alterações em habilidades

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,5 @@
-function mudarImagem(input){
-    document.getElementById("picture").src = input;
-}
+barras = document.querySelectorAll(".barras");
 
-var imagens = ["images/Cplusplus.png","images/CSS3.png","images/girl.jpg"];
-
-let i = 0;
-function iterarImagens(){
-    if(i < 2) {
-        i++;
-    }else{
-        i = 0;
-    }
-    document.getElementById("picture").src = imagens[i];
-    setTimeout(iterarImagens,2000);
+for(n in barras){
+    n.style.width = "10%";
 }
